ebf59ec0e0f81d7c82380ed69a68809e79dcd708
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-10-07 11:34:45 -0700

    Opción de esconder las preguntas en la pantalla del historial de scores.

--- app/src/main/java/com/leonardolarranaga/kotlintrivia/presentation/home/roundscores/RoundScoreCard.kt
+++ app/src/main/java/com/leonardolarranaga/kotlintrivia/presentation/home/roundscores/RoundScoreCard.kt
@@ -10,14 +10,24 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -37,6 +47,7 @@ fun RoundScoreCard(
     index: Int
 ) {
     val pagerState = rememberPagerState(pageCount = { roundScore.questions.size })
+    val showQuestions = remember { mutableStateOf(false) }
 
     OutlinedCard(
         modifier = modifier.fillMaxWidth(),
@@ -74,48 +85,64 @@ fun RoundScoreCard(
                 )
             }
 
-            HorizontalPager(state = pagerState) { page ->
-                val question = roundScore.questions[page]
+            Button(
+                onClick = { showQuestions.value = !showQuestions.value },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentSize(Alignment.CenterEnd)
+                    .padding(end = 5.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.kotlin_purple_blue)
+                )
+            ) {
+                Icon(
+                    imageVector = if (showQuestions.value) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Ocultar/Mostrar preguntas")
+            }
 
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .padding(horizontal = 2.dp)
-                        .background(
-                            color = if (question.correctAnswer == question.selectedAnswer)
-                                colorResource(id = R.color.iguana_green)
-                            else
-                                colorResource(id = R.color.kotlin_rose).copy(0.9f),
-                            shape = RoundedCornerShape(10)
-                        )
-                        .height(200.dp)
-                        .padding(10.dp)
-                ) {
-                    Text(
-                        text = question.question,
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.Bold
-                    )
+            if (showQuestions.value)
+                HorizontalPager(state = pagerState) { page ->
+                    val question = roundScore.questions[page]
 
-                    Text(
-                        text = question.correctAnswer,
-                        style = MaterialTheme.typography.titleMedium,
-                        fontWeight = FontWeight.Black
-                    )
-
-                    if (question.correctAnswer != question.selectedAnswer) {
-                        HorizontalDivider(
-                            color = Color.DarkGray,
-                            modifier = Modifier.padding(vertical = 5.dp)
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .padding(horizontal = 2.dp)
+                            .background(
+                                color = if (question.correctAnswer == question.selectedAnswer)
+                                    colorResource(id = R.color.iguana_green)
+                                else
+                                    colorResource(id = R.color.kotlin_rose).copy(0.9f),
+                                shape = RoundedCornerShape(10)
+                            )
+                            .height(200.dp)
+                            .padding(10.dp)
+                    ) {
+                        Text(
+                            text = question.question,
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Bold
                         )
 
                         Text(
-                            text = "Tu respuesta: ${question.selectedAnswer}",
+                            text = question.correctAnswer,
                             style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Black
                         )
+
+                        if (question.correctAnswer != question.selectedAnswer) {
+                            HorizontalDivider(
+                                color = Color.DarkGray,
+                                modifier = Modifier.padding(vertical = 5.dp)
+                            )
+
+                            Text(
+                                text = "Tu respuesta: ${question.selectedAnswer}",
+                                style = MaterialTheme.typography.titleMedium,
+                            )
+                        }
                     }
                 }
-            }
         }
     }
 }
